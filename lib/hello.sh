cd12c0fc5206dbf2282ecaa107f7f3ce116ad5e0
emam mohamed <em991685@gmail.com>
2026-01-15 04:48:53 +0200

    Update hello.sh to use greeter

--- lib/hello.sh
+++ lib/hello.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Default is "World"
-name=${1:-"World"}
-echo "Hello, $name"
-# Default is World
-# Author: Jim Weirich (jim@example.com)
+source lib/greeter.sh
+
+name="$1"
+if [ -z "$name" ]; then
+    name="World"
+fi
+
+Greeter "$name"
